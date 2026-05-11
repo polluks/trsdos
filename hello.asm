310d0dad385ec3db3210c2ddb6b7f0b9d8247340
Stefan Haubenthal <polluks@sdf.org>
2026-05-11 16:29:25 +0200

    Add hello.asm - HELLO/CMD auto-run command file

---
+++ hello.asm
@@ -0,0 +1,8 @@
+; HELLO/CMD - TRSDOS auto-run command file
+; Loaded to $3000 by LSDOS after SYSRES init
+; Displays sign-on message, then returns to DOS READY prompt
+
+    ORG 3000H
+
+START:
+    RET
