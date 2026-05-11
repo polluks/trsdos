c5f1141448cc61046c2aa7ca17cbc5aa1da81150
Stefan Haubenthal <polluks@sdf.org>
2026-05-11 13:29:43 +0200

    Move 40-col mode check to 6502 boot sector via KERNAL
    Use KERNAL I_80COL ($FF5F) and CHROUT ($FFD2) to detect and warn about 40-column mode before switching to Z80. Removes the Z80-side 40-col warning (now handled in the boot block).

--- boot_sector.s
+++ boot_sector.s
@@ -1,6 +1,7 @@
 ; C128 TRSDOS 6502 Boot Sector - vasm6502_oldstyle syntax
 ; Uses proper C128 DISKHDR format: KERNAL auto-loads Z80BOOT PRG,
-; then JSRs to our code which switches to Z80 mode.
+; then JSRs to our code which checks for 40/80 column mode,
+; warns if in 40-col, then switches to Z80 mode.
 
 	ORG	$0B00
 
@@ -10,14 +11,34 @@
 	DB	0		; $0B05: bank (unused when addr=0)
 	DB	0		; $0B06: blk# (0 = no extra sectors)
 
-; Boot message (KERNAL displays "BOOTING Z80BOOT...")
-	DB	"TRSDOS",0	; $0B07-$0B0C: displayed as "BOOTING TRSDOS..."
+; Boot message (KERNAL displays "BOOTING <msg>...")
+	DB	"TRSDOS",0	; $0B07-$0B0C: displayed during boot
 
 ; PRG filename to auto-load (KERNAL LOADs it to bank 0)
 	DB	"Z80BOOT",0	; $0B0E-$0B15
 
+; KERNAL vectors
+I_80COL	EQU	$FF5F
+CHROUT	EQU	$FFD2
+
 ; Code entry at $0B16: KERNAL JSRs here after loading Z80BOOT
-	; Set Z80 boot vector at $FFF0: JP $4300
+	CLC			; read current 80-column mode
+	JSR	I_80COL		; A=0 = 40-col, A!=0 = 80-col
+	BNE	set_z80
+
+	; In 40-column mode: warn user then switch to 80-col
+	LDX	#$00		; message index
+pmsg:	LDA	msg40,X
+	BEQ	pmsg_e
+	JSR	CHROUT
+	INX
+	BNE	pmsg
+
+pmsg_e:	SEC			; set 80-column mode
+	LDA	#$01
+	JSR	I_80COL
+
+set_z80:
 	LDA	#$C3
 	STA	$FFF0
 	LDA	#$00
@@ -25,8 +46,9 @@
 	LDA	#$43
 	STA	$FFF2
 
-	; Enable Z80 clock, configuration 0
 	LDA	#$B0
 	STA	$D505
 
 	RTS
+
+msg40:	DB	"SWITCH TO 80-COL MODE",13,0
